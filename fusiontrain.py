b3de4363150b49d506a08e860cfb61b47eb2ce68
Baixuzx7 <43493906+Baixuzx7@users.noreply.github.com>
2022-06-27 20:48:49 +0800

    Update fusiontrain.py

--- fusiontrain.py
+++ fusiontrain.py
@@ -14,7 +14,7 @@ from Eval import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_epochs', type=int, default=1000)
-parser.add_argument('--batch_size', type=int, default=2)
+parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--lr', type=float, default=3e-4)
 parser.add_argument('--decay', type=float, default=0.9)
 parser.add_argument('--eps', type=float, default=1e-3)
